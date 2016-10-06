95947e5eae4931ae32ccde863bcba6da5a35d5d2
machaneus <machaneus@gmail.com>
2016-10-07 01:29:11 +0300

    Remove prints, add tweets per state bar graph

--- tweetanalysis.py
+++ tweetanalysis.py
@@ -1,13 +1,24 @@
 import json
 import sys
+import matplotlib.pyplot as plt
+from states import state_abbreviations,getStateFromLocationString
 
 if __name__ == "__main__":
+    state_tweets = dict.fromkeys(state_abbreviations, 0)
     if len(sys.argv) < 2:
         print 'Usage: need to provide tweets filename!'
         exit()
     tweets_file = sys.argv[1]
     with open(tweets_file) as tweets:
         for tweet in tweets.readlines():
-            print json.loads(tweet)['user']['location'], '\t\t', json.loads(tweet)['text']
+            loc = json.loads(tweet)['user']['location']
+            state = getStateFromLocationString(loc)
+            state_tweets[state]+=1
+
+    plt.bar(range(len(state_tweets)), state_tweets.values(), align='center')
+    plt.xticks(range(len(state_tweets)), state_tweets.keys())
+
+    plt.show()
+
 
 
