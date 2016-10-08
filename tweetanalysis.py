e11c9b5baac3e00b7a32e6bd886e55b63ef9cf99
machaneus <machaneus@gmail.com>
2016-10-08 16:24:41 +0300

    Implement AFINN analyzer

--- tweetanalysis.py
+++ tweetanalysis.py
@@ -3,32 +3,43 @@ import sys
 import matplotlib.pyplot as plt
 from states import state_abbreviations,getStateFromLocationString
 
-def loadSentimentFile(filename):
-    with open(filename) as sentimentFile:
-        scores={}
-        for line in sentimentFile:
-            term, score = line.split("\t")
-            scores[term] = int(score)
+class AFINNSentimentAnalyzer(object):
 
-    return scores
+    def __init__(self, filename):
+        self.scores = self.loadSentimentFile(filename)
+   
+    def loadSentimentFile(self, filename):
+        with open(filename) as sentimentFile:
+            scores={}
+            for line in sentimentFile:
+                term, score = line.split("\t")
+                scores[term] = int(score)
 
-def getTextScore(text, sentiment_scores):
-    score = 0
-    words = text.split(' ')
-    for word in words:
-        if word in sentiment_scores:
-            score+=sentiment_scores[word]
-    return score
+        return scores
+
+    def getTextSentiment(self,text):
+        score = 0
+        words = text.split(' ')
+        for word in words:
+            if word in self.scores:
+                score+=self.scores[word]
+        return score
+
+
 
 if __name__ == "__main__":
     state_tweets = dict.fromkeys(state_abbreviations, 0)
     state_scores = dict.fromkeys(state_abbreviations, 0)
+
 
     if len(sys.argv) < 3:
         print 'Usage: need to provide tweets and scores filenames!'
         exit()
     tweets_file = sys.argv[1]
     scores_file = sys.argv[2]
+
+    analyzer = AFINNSentimentAnalyzer(scores_file)
+
     with open(tweets_file) as tweets:
         for tweet in tweets.readlines():
             loc = json.loads(tweet)['user']['location']
@@ -37,7 +48,7 @@ if __name__ == "__main__":
             state = getStateFromLocationString(loc)
             state_tweets[state]+=1
 
-            score = getTextScore(text, loadSentimentFile(scores_file))
+            score = analyzer.getTextSentiment(text)
             state_scores[state]+=score
 
             
